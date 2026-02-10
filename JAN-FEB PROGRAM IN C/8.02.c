173b0046aebbe020a963c5fb3cda5aaca2d029ff
keerthika <keerthi6836@gmail.com>
2026-02-10 18:58:08 +0530

    Day-9 completed

---
+++ JAN-FEB PROGRAM IN C/8.02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int v1, v2, v3;
+    float average;
+    scanf("%d %d %d", &a, &b, &c);
+    average = (a + b + c) / 3.0;
+    printf("Average = %.2F", average);
+    return 0;
+}
---
+++ JAN-FEB PROGRAM IN PYTHON/8.02.py
@@ -0,0 +1,3 @@
+v1, v2, v3 = map(int, input("Enter three numbers: ").split())
+average = (v1 + v2 + v3) / 3.0
+print(f"Average = {average:.2f}")
