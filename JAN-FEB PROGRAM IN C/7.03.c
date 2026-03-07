ec77f746ddcafad5478dc52d05de1f8ab5027004
keerthika <keerthi6836@gmail.com>
2026-03-07 13:48:14 +0530

    Day-11 completed

---
+++ JAN-FEB PROGRAM IN C/7.03.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int i = 1;
+    printf("Counting from 1 to 5:\n");
+    while (i <= 5) {
+        printf("%d", i);
+        i++;
+    }
+    return 0;
+}
---
+++ JAN-FEB PROGRAM IN PYTHON/7.03.py
@@ -0,0 +1,7 @@
+i = 1
+
+print("Counting from 1 to 5:")
+
+while i <= 5:
+    print(i)
+    i += 1
